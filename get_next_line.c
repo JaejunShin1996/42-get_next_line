5edec9beaed693b6b2ba26135eb46cd4b9391d5b
Jaejun Shin <jaeshin@F2N051.42adel.org.au>
2023-07-12 16:17:18 +0930

    Norm Updated

--- get_next_line.c
+++ get_next_line.c
@@ -1,106 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 16:02:38 by jaeshin           #+#    #+#             */
+/*   Updated: 2023/07/12 16:10:09 by jaeshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *join_free(char *buffer, char *temp)
+char	*join_free(char *buffer, char *temp)
 {
-    char    *joined;
+	char	*joined;
 
-    joined = ft_strjoin(buffer, temp);
-    free(buffer);
-    return (joined);
+	joined = ft_strjoin(buffer, temp);
+	free(buffer);
+	return (joined);
 }
 
-char    *get_chunk_strings(int fd, char *buffer)
+char	*get_chunk_strings(int fd, char *buffer)
 {
-    char    *temp;
-    int     read_size;
+	char	*temp;
+	int		read_size;
 
-    // First buffer is empty.
-    if (!buffer)
-        buffer = ft_calloc(1, sizeof(char));
-    temp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-    read_size = 1;
-    while (read_size > 0)
-    {
-        // reads the file using size given.
-        read_size = read(fd, temp, BUFFER_SIZE);
-        if (read_size == -1)
-        {
-            free(temp);
-            return (NULL);
-        }
-        temp[read_size] = '\0';
-        buffer = join_free(buffer, temp);
-        // if it contains a new line, then break.
-        if (ft_strchr(buffer, '\n'))
-            break;
-    }
-    free(temp);
-    return (buffer);
+	// First buffer is empty.
+	if (!buffer)
+	buffer = ft_calloc(1, sizeof(char));
+	temp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	read_size = 1;
+	while (read_size > 0)
+	{
+		// reads the file using size given.
+		read_size = read(fd, temp, BUFFER_SIZE);
+		if (read_size == -1)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp[read_size] = '\0';
+		buffer = join_free(buffer, temp);
+		// if it contains a new line, then break.
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(temp);
+	return (buffer);
 }
 
-char    *get_return_line(char *chunk)
+char	*get_return_line(char *chunk)
 {
-    char    *result;
-    size_t  i;
+	char	*result;
+	size_t	i;
 
-    i = 0;
-    if (!chunk[i])
-        return (NULL);
-    // size of line upto new line.
-    while (chunk[i] && chunk[i] != '\n')
-        i++;
-    // allocates the memory based on the size.
-    result = (char *)ft_calloc(i + 2, sizeof(char));
-    i = 0;
-    // copies the value.
-    while (chunk[i] && chunk[i] != '\n')
-    {
-        result[i] = chunk[i];
-        i++;
-    }
-    if (chunk[i] && chunk[i] == '\n')
-        result[i] = '\n';
-    return (result);
+	i = 0;
+	if (!chunk[i])
+		return (NULL);
+	// size of line upto new line.
+	while (chunk[i] && chunk[i] != '\n')
+		i++;
+	// allocates the memory based on the size.
+	result = (char *)ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	// copies the value.
+	while (chunk[i] && chunk[i] != '\n')
+	{
+		result[i] = chunk[i];
+		i++;
+	}
+	if (chunk[i] && chunk[i] == '\n')
+		result[i] = '\n';
+	return (result);
 }
 
-char    *get_ready_for_next(char *chunk)
+char	*get_ready_for_next(char *chunk)
 {
-    char    *result;
-    int     i;
-    int     j;
+	char	*result;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    // size upto new line.
-    while (chunk[i] && chunk[i] != '\n')
-        i++;
-    if (!chunk[i])
-    {
-        free(chunk);
-        return (NULL);
-    }
-    // allocates the size and copies the rest of the chunk.
-    result = (char *)ft_calloc(ft_strlen(chunk) - i + 1, sizeof(char));
-    i++;
-    while (chunk[i])
-        result[j++] = chunk[i++];
-    result[j] = '\0';
-    free(chunk);
-    return (result);
+	i = 0;
+	j = 0;
+	//size upto new line.
+	while (chunk[i] && chunk[i] != '\n')
+		i++;
+	if (!chunk[i])
+	{
+		free(chunk);
+		return (NULL);
+	}
+	//allocates the size and copies the rest of the chunk.
+	result = (char *)ft_calloc(ft_strlen(chunk) - i + 1, sizeof(char));
+	i++;
+	while (chunk[i])
+		result[j++] = chunk[i++];
+	result[j] = '\0';
+	free(chunk);
+	return (result);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *buffer;
-    char        *line;
+	static char	*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-    // gets a chunk of strings that contains new line.
-    buffer = get_chunk_strings(fd, buffer);
-    // gets a line including the new line from the chunk.
-    line = get_return_line(buffer);
-    // saves the rest of the chunk excluding the line to be returned.
-    buffer = get_ready_for_next(buffer);
-    return (line);
+	//gets a chunk of strings that contains new line.
+	buffer = get_chunk_strings(fd, buffer);
+	//gets a line including the new line from the chunk.
+	line = get_return_line(buffer);
+	//saves the rest of the chunk excluding the line to be returned.
+	buffer = get_ready_for_next(buffer);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,102 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 15:57:38 by jaeshin           #+#    #+#             */
+/*   Updated: 2023/07/12 16:16:04 by jaeshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-void    ft_putchar_fd(char c, int fd)
+void	*ft_memset(void *str, int c, size_t n)
 {
-    write(fd, &c, 1);
+	size_t			i;
+	unsigned char	*temp;
+
+	i = 0;
+	temp = (unsigned char *)str;
+	while (i < n)
+	{
+		temp[i] = c;
+		i++;
+	}
+	return (str);
 }
 
-void    ft_putstr_fd(char *s, int fd)
+void	*ft_calloc(size_t nitems, size_t size)
 {
-    while (*s)
-    {
-        ft_putchar_fd(*s, fd);
-        s++;
-    }
+	void	*result;
+
+	if (nitems == 0 || size == 0)
+		return (malloc(0));
+	result = (void *)malloc(nitems * size);
+	if (!result)
+		return (NULL);
+	ft_memset((unsigned char *)result, 0, nitems * size);
+	return (result);
 }
 
-void    *ft_memset(void *str, int c, size_t n)
+size_t	ft_strlen(const char *str)
 {
-    size_t          i;
-    unsigned char   *temp;
-    
-    i = 0;
-    temp = (unsigned char *)str;
-    while (i < n)
-    {
-        temp[i] = c;
-        i++;
-    }
-    return (str);
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-void    ft_bzero(void *str, size_t n)
+char	*ft_strjoin(char const *str1, char const *str2)
 {
-    ft_memset(str, 0, n);
+	char	*result;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	result = malloc(sizeof(char) * (ft_strlen(str1) + ft_strlen(str2) + 1));
+	if (!result || !str1 || !str2)
+		return (NULL);
+	while (str1[i])
+	{
+		result[i] = str1[i];
+		i++;
+	}
+	while (str2[j])
+	{
+		result[i + j] = str2[j];
+		j++;
+	}
+	result[i + j] = '\0';
+	return (result);
 }
 
-void    *ft_calloc(size_t nitems, size_t size)
+char	*ft_strchr(const char *str, int c)
 {
-    void    *result;
- 
-    result = malloc(nitems * size);
-    if (result == NULL)
-        return (NULL);
-    ft_bzero(result, nitems * size);
-    return (result);
-}
-
-int read_fd(char *file_name)
-{
-    int     fd;
-
-    fd = open(file_name, O_RDONLY);
-    return (fd);
-}
-
-size_t  ft_strlen(const char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-
-char    *ft_strjoin(char const *str1, char const *str2)
-{
-    char    *result;
-    int     i;
-    int     j;
-
-    i = 0;
-    j = 0;
-    result = (char *)malloc(sizeof(char) * (ft_strlen(str1) + ft_strlen(str2) + 1));
-    if (!result || !str1 || !str2)
-        return (NULL);
-    while (str1[i])
-    {
-        result[i] = str1[i];
-        i++;
-    }
-    while (str2[j])
-    {
-        result[i + j] = str2[j];
-        j++;
-    }
-    result[i + j] = '\0';
-    return (result);
-}
-
-char    *ft_strchr(const char *str, int c)
-{
-    while (*str)
-    {
-        if (*str == c)
-            return ((char *)str);
-        str++;
-    }
-    if (*str == c)
-        return ((char *)str);
-    return (NULL);
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if (*str == (char)c)
+		return ((char *)str);
+	return (NULL);
 }
